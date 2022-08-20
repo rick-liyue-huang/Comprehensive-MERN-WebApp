b3ece5b10f9ce9d1f0ccb3a0752d490e089e38da
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-08-20 19:06:02 +1000

    ci: add frontend

---
+++ front-end/src/features/notes/NotesList.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const NotesList = () => {
+  return <h1>NotesList</h1>;
+};
---
+++ front-end/src/features/users/UsersList.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const UsersList = () => {
+  return <h1>UsersList</h1>;
+};
